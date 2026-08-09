0d67166d73e84730b197602f23d4963ed513665b
Ananya Singh <ananyasingh17sep@gmail.com>
2026-08-09 20:54:03 +0530

    Time: 6 ms (55.41%), Space: 47.2 MB (88.39%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
@@ -1,32 +1,54 @@
 class Solution {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
         
-        int n1=nums1.length;
-        int n2=nums2.length;
-        int a=0; int b=0;
+        // int n1=nums1.length;
+        // int n2=nums2.length;
+        // int a=0; int b=0;
 
-        for(int i=0; i<n1;i++)
-        {
-            for(int j=0;j<n2;j++)
-            {
-                if(nums1[i]==nums2[j])
-                {
-                    a++;
-                    break;
-                }
-            }
-        }
-        for(int i=0; i<n2;i++)
-        {
-            for(int j=0;j<n1;j++)
-            {
-                if(nums1[j]==nums2[i]){
-                    b++;
-                    break;}
-            }
-        }
+        // for(int i=0; i<n1;i++)
+        // {
+        //     for(int j=0;j<n2;j++)
+        //     {
+        //         if(nums1[i]==nums2[j])
+        //         {
+        //             a++;
+        //             break;
+        //         }
+        //     }
+        // }
+        // for(int i=0; i<n2;i++)
+        // {
+        //     for(int j=0;j<n1;j++)
+        //     {
+        //         if(nums1[j]==nums2[i]){
+        //             b++;
+        //             break;}
+        //     }
+        // }
 
-            return  new int[]{a,b};
+        //     return  new int[]{a,b};
+
+
+        HashSet<Integer> s1=new HashSet<>();
+    HashSet<Integer> s2=new HashSet<>();
+    
+    int a=0;int b=0;
+     for(int x:nums1)
+     s1.add(x);
+     
+     for(int x:nums2)
+     s2.add(x);
+      
+     for(int x:nums1)
+     if(s2.contains(x))
+     a++;
+     
+     for(int x:nums2)
+     if(s1.contains(x))
+     b++;
+     
+     return new int[]{a,b};
+        
 
     }
 }
